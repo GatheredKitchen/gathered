65d257a4a623bb86b2b35b3432cd01085a2676e1
GatheredKitchen <hello@usegathered.app>
2026-05-05 17:55:22 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/login',
+  build: {
+    rollupOptions: {
+      input: {
+        app: 'login.html',
+      },
+    },
+  },
 })
